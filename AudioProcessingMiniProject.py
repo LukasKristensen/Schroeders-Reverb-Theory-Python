10fbbf4e7aeec4b37f0e452f3f9469bb4f6b662f
DKtruster <lukashtxb16@gmail.com>
2021-05-13 02:57:07 +0200

    gui.done

--- AudioProcessingMiniProject.py
+++ AudioProcessingMiniProject.py
@@ -8,9 +8,10 @@ from numpy import random
 import scipy.io.wavfile
 import audioFilters
 
-defaultValues = [["0.10","0.12","0.14","0.16"],["0.2","0.22","0.24","0.26"],["0.30","0.32","0.34","0.36"],["0.40","0.42","0.44","0.46"]]
+defaultValues = [[0.3, 0.25, 0.25, 0.2], [1553, 1613, 1493, 1153], [223, 443], [-0.7, -0.7],[0.7]]
 
-currentThreads = []
+currentThreadsInputPlay = []
+currentThreadsOutputPlay = []
 
 mixingGUI = []
 mixingLABEL = []
@@ -21,12 +22,17 @@ plainReverbLABEL = []
 plainReverbAmpGUI = []
 plainReverbAmpLABEL = []
 
-allPassGUI = []
-allPassLABEL = []
+allPassDelayGUI = []
+allPassDelayLABEL = []
+
+allPassParamGUI = []
+allPassParamLABEL = []
 
 playing = False
 soundfileInfo = []
 
+matplotAwait = [True]
+matplotData = []
 
 def matplobLib():
     samplerate, data = scipy.io.wavfile.read("africa-toto.wav", mmap=False )
@@ -42,38 +48,44 @@ def matplobLib():
 
     emptyarray=np.array(emptyarray)
 
-    plt.figure(1)
+    while matplotAwait[0]:
+        ""
+
+    plt.figure("Input and output comparison")
     plt.title("Graph for .wav file")
-    plt.plot(data)
-    plt.plot(listHalf)
-    plt.plot(listHalfHalf)
-    plt.plot(emptyarray)
+    plt.plot(matplotData[1])
+    plt.plot(matplotData[0])
 
     scipy.io.wavfile.write("testing-africa.wav",samplerate,emptyarray.astype(np.dtype('i2')))
     plt.show()
 
-def randomPreset():
-    for i in range (len(mixingGUI)):
-        mixingGUI[i].set(random.rand())
-        plainReverbAmpGUI[i].set(random.rand())
-        plainReverbGUI[i].set(random.rand())
-        allPassGUI[i].set(random.rand())
-
 def standardPresetOne():
-    for i in range (len(mixingGUI)):
-        mixingGUI[i].set(defaultValues[0][i])
-        plainReverbAmpGUI[i].set(defaultValues[1][i])
-        plainReverbGUI[i].set(defaultValues[2][i])
-        allPassGUI[i].set(defaultValues[3][i])
+    for i, data in enumerate(defaultValues[0]):
+        mixingGUI[i].set(data)
+    for i, data in enumerate(defaultValues[1]):
+        plainReverbGUI[i].set(data)
+    for i, data in enumerate(defaultValues[2]):
+        allPassDelayGUI[i].set(data)
+    for i, data in enumerate(defaultValues[3]):
+        allPassParamGUI[i].set(data)
+    for i, data in enumerate(defaultValues[4]):
+        plainReverbAmpGUI[i].set(data)
 
 def getValues():
-    storedVals = [[], [], [], []]
+    storedVals = [[], [], [], [],[]]
 
-    for i in range(len(mixingGUI)):
-        storedVals[0].append(mixingGUI[i].get())
-        storedVals[1].append(plainReverbAmpGUI[i].get())
-        storedVals[2].append(plainReverbGUI[i].get())
-        storedVals[3].append(allPassGUI[i].get())
+    for i in mixingGUI:
+        storedVals[0].append(i.get())
+    for i in plainReverbAmpGUI:
+        storedVals[1].append(i.get())
+    for i in plainReverbGUI:
+        storedVals[2].append(i.get())
+    for i in allPassDelayGUI:
+        storedVals[3].append(i.get())
+    for i in plainReverbAmpGUI:
+        storedVals[4].append(i.get())
+
+    print("storedVals",storedVals)
     return storedVals
 
 
@@ -81,8 +93,12 @@ def processAudioPass():
     valueParameters = getValues()
 
     processedAudio = audioFilters.schroedersReverb(soundfileInfo[0], valueParameters, soundfileInfo[1])
+    scipy.io.wavfile.write("africa-toto-output.wav", soundfileInfo[1], processedAudio.astype(np.dtype('i2')))
 
-    scipy.io.wavfile.write("DAY3-africa7.wav",soundfileInfo[1],processedAudio.astype(np.dtype('i2')))
+    matplotData.append(soundfileInfo[0])
+    matplotData.append(processedAudio)
+    matplotAwait[0] = False
+
     return processedAudio
 
 
@@ -99,64 +115,60 @@ def displayGUI():
     plainReverbLabel = tk.Label(text="Plain reverb parameters")
     allPassLabel = tk.Label(text="Allpass parameters")
 
-    playAudio = tk.Button(text="Play original .wav file", width="20", height="5", bg=bgC, fg="black", command=threadStart)
-    stopAudio = tk.Button(text="Stop .wav original", width="20", height="5", bg=bgC, fg="black", command=threadStop)
+    playAudio = tk.Button(text="Play original .wav file", width="20", height="5", bg=bgC, fg="black", command=threadStartInput)
+    stopAudio = tk.Button(text="Stop .wav original", width="20", height="5", bg=bgC, fg="black", command=threadStopInput)
     processAudio = tk.Button(text="Start Processing", width="20", height="5", bg=bgC, fg="black", command=processAudioPass)
-    playNewAudio = tk.Button(text="Play new .wav file", width="20", height="5", bg=bgC, fg="black",command="")
-    stopNewAudioAudio = tk.Button(text="Stop new .wav original", width="20", height="5", bg=bgC, fg="black", command="")
+    playNewAudio = tk.Button(text="Play new .wav file", width="20", height="5", bg=bgC, fg="black",command=threadStartOutput)
+    stopNewAudioAudio = tk.Button(text="Stop new .wav original", width="20", height="5", bg=bgC, fg="black", command=threadStopOutput)
 
-    defaultPreset = tk.Button(text="Random preset", width="20", height="5", bg=bgC, fg="black", command=randomPreset)
     standardPreset = tk.Button(text="Standard preset", width="20", height="5", bg=bgC, fg="black", command=standardPresetOne)
     getValue = tk.Button(text="GetValues", width="20", height="5", bg=bgC, fg="black", command=getValues)
 
-    s1label = tk.Label(text="s1")
-    s2label = tk.Label(text="s2")
-    s3label = tk.Label(text="s3")
-    s4label = tk.Label(text="s4")
-    mixingLABEL.extend([s1label, s2label, s3label,s4label])
+    mixingParamLabel1 = tk.Label(text="mixing1")
+    mixingParamLabel2 = tk.Label(text="mixing2")
+    mixingParamLabel3 = tk.Label(text="mixing3")
+    mixingParamLabel4 = tk.Label(text="mixing4")
+    mixingLABEL.extend([mixingParamLabel1, mixingParamLabel2, mixingParamLabel3,mixingParamLabel4])
 
-    s1scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    s2scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    s3scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    s4scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    mixingGUI.extend([s1scale, s2scale, s3scale,s4scale])
+    mixingParamScale1 = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
+    mixingParamScale2 = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
+    mixingParamScale3 = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
+    mixingParamScale4 = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
+    mixingGUI.extend([mixingParamScale1, mixingParamScale2, mixingParamScale3,mixingParamScale4])
 
-    d1label = tk.Label(text="d1")
-    d2label = tk.Label(text="d2")
-    d3label = tk.Label(text="d3")
-    d4label = tk.Label(text="d4")
-    plainReverbLABEL.extend([d1label, d2label, d3label,d4label])
+    reverbDelayLabel1 = tk.Label(text="reverbDelay1")
+    reverbDelayLabel2 = tk.Label(text="reverbDelay2")
+    reverbDelayLabel3 = tk.Label(text="reverbDelay3")
+    reverbDelayLabel4 = tk.Label(text="reverbDelay4")
+    plainReverbLABEL.extend([reverbDelayLabel1, reverbDelayLabel2, reverbDelayLabel3, reverbDelayLabel4])
 
-    d1scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    d2scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    d3scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    d4scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    plainReverbGUI.extend([d1scale, d2scale, d3scale,d4scale])
+    reverbDelayScale1 = tk.Scale(from_=0, to=2000, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
+    reverbDelayScale2 = tk.Scale(from_=0, to=2000, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
+    reverbDelayScale3 = tk.Scale(from_=0, to=2000, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
+    reverbDelayScale4 = tk.Scale(from_=0, to=2000, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
+    plainReverbGUI.extend([reverbDelayScale1, reverbDelayScale2, reverbDelayScale3, reverbDelayScale4])
 
-    a1label = tk.Label(text="a1")
-    a2label = tk.Label(text="a2")
-    a3label = tk.Label(text="a3")
-    a4label = tk.Label(text="a4")
-    plainReverbAmpLABEL.extend([a1label, a2label, a3label,a4label])
+    reverbTimeLabel1 = tk.Label(text="reverbTime")
+    plainReverbAmpLABEL.extend([reverbTimeLabel1])
 
-    a1scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    a2scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    a3scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    a4scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    plainReverbAmpGUI.extend([a1scale, a2scale, a3scale,a4scale])
+    reverbTimeScale1 = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
+    plainReverbAmpGUI.extend([reverbTimeScale1])
 
-    a5label = tk.Label(text="a5")
-    a6label = tk.Label(text="a6")
-    d5label = tk.Label(text="d5")
-    d6label = tk.Label(text="d6")
-    allPassLABEL.extend([a5label, a6label, d5label,d6label])
+    a5label = tk.Label(text="ReverbDelay1")
+    a6label = tk.Label(text="ReverbDelay2")
+    allPassDelayLABEL.extend([a5label, a6label])
 
-    a5scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    a6scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    d5scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    d6scale = tk.Scale(from_=0, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
-    allPassGUI.extend([a5scale, a6scale, d5scale,d6scale])
+    d5label = tk.Label(text="AllpassParam1")
+    d6label = tk.Label(text="AllpassParam2")
+    allPassParamLABEL.extend([d5label, d6label])
 
+    a5scale = tk.Scale(from_=0, to=2000, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
+    a6scale = tk.Scale(from_=0, to=2000, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
+    allPassDelayGUI.extend([a5scale, a6scale])
+
+    d5scale = tk.Scale(from_=-0.99, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
+    d6scale = tk.Scale(from_=-0.99, to=0.99, digits=2, resolution=0.01, bg=bgC, fg="black", orient="horizontal")
+    allPassParamGUI.extend([d5scale, d6scale])
 
     for i in range(len(mixingGUI)):
         # mixingGUI[i].bind("<Button-1>")
@@ -169,19 +181,21 @@ def displayGUI():
         plainReverbLABEL[i].grid(row=(i*2)+2,column=1,pady=0,sticky="N")
     plainReverbLabel.grid(row = 0, column=1, pady =5)
 
-    for i in range(len(plainReverbAmpGUI)):
-        plainReverbAmpGUI[i].grid(row=(i*2)+1,column=2,pady=10,sticky="N")
-        plainReverbAmpLABEL[i].grid(row=(i*2)+2,column=2,pady=0,sticky="N")
+    plainReverbAmpGUI[0].grid(row=1,column=2,pady=10,sticky="N")
+    plainReverbAmpLABEL[0].grid(row=2,column=2,pady=0,sticky="N")
 
     allPassLabel.grid(row = 0, column=3, pady =5)
-    for i in range(len(plainReverbAmpGUI)):
-        allPassGUI[i].grid(row=(i*2)+1,column=3,pady=10,sticky="N")
-        allPassLABEL[i].grid(row=(i*2)+2,column=3,pady=0,sticky="N")
 
-    defaultPreset.grid(row = 11, column=1, pady =40)
+    for i in range(len(allPassDelayGUI)):
+        allPassDelayGUI[i].grid(row=(i*2)+1,column=3,pady=10,sticky="N")
+        allPassDelayLABEL[i].grid(row=(i*2)+2,column=3,pady=0,sticky="N")
+
+    for i in range(len(allPassParamGUI)):
+        allPassParamGUI[i].grid(row=(i*2)+5,column=3,pady=10,sticky="N")
+        allPassParamLABEL[i].grid(row=(i*2)+6,column=3,pady=0,sticky="N")
+
     standardPreset.grid(row=11,column=2, pady=40)
     getValue.grid(row=11,column=3,pady=40)
-
 
     playAudio.grid(row = 12, column=1, pady =40)
     stopAudio.grid(row = 12, column=2, pady =40)
@@ -192,7 +206,7 @@ def displayGUI():
     audioGUIWindow.mainloop()
 
 
-def playWav():
+def playOriginalWav():
     fileName = "africa-toto.wav"
     wavRead = wave.open(fileName, "rb")
     print("Status: Currently playing",fileName)
@@ -207,34 +221,61 @@ def playWav():
                            rate=frameRate,
                            output=True)
     data = wavRead.readframes(frameRate)
-    while data and len(currentThreads) != 0:
+    while data and len(currentThreadsInputPlay) != 0:
         audioStream.write(data)
         data = wavRead.readframes(frameRate)
 
 
-def threadStart():
-    holdThreadSize = len(currentThreads)
-    currentThreads.append(threading.Thread(target=playWav))
-    currentThreads[holdThreadSize].start()
+def playOutputWav():
+    fileName = "africa-toto-output.wav"
+    wavRead = wave.open(fileName, "rb")
+    print("Status: Currently playing",fileName)
+
+    pyA = pyaudio.PyAudio()
+
+    frameRate = wavRead.getframerate()
+    print("frameRate:",frameRate)
+
+    audioStream = pyA.open(format=pyA.get_format_from_width(wavRead.getsampwidth()),
+                           channels=wavRead.getnchannels(),
+                           rate=frameRate,
+                           output=True)
+    data = wavRead.readframes(frameRate)
+    while data and len(currentThreadsOutputPlay) != 0:
+        audioStream.write(data)
+        data = wavRead.readframes(frameRate)
+
+def threadStartInput():
+    holdThreadSize = len(currentThreadsInputPlay)
+    currentThreadsInputPlay.append(threading.Thread(target=playOriginalWav))
+    currentThreadsInputPlay[holdThreadSize].start()
 
 
-def threadStop():
-    if len(currentThreads) != 0:
-        currentThreads.pop(len(currentThreads) - 1)
-        print("Status: Stopped. Total threads -", len(currentThreads))
+def threadStopInput():
+    if len(currentThreadsInputPlay) != 0:
+        currentThreadsInputPlay.pop(len(currentThreadsInputPlay) - 1)
+        print("Status: Stopped. Total threads -", len(currentThreadsInputPlay))
 
 
-fileName = "africa-toto.wav"
-wavRead = wave.open(fileName, "rb")
+def threadStartOutput():
+    holdThreadSize = len(currentThreadsOutputPlay)
+    currentThreadsOutputPlay.append(threading.Thread(target=playOutputWav))
+    currentThreadsOutputPlay[holdThreadSize].start()
 
-frameRate = wavRead.getframerate()
+
+def threadStopOutput():
+    if len(currentThreadsOutputPlay) != 0:
+        currentThreadsOutputPlay.pop(len(currentThreadsOutputPlay) - 1)
+        print("Status: Stopped. Total threads -", len(currentThreadsOutputPlay))
+
 
 samplerate, data = scipy.io.wavfile.read("day3_testing_sample.wav", mmap=False)
 
 soundfileInfo.append(data)
 soundfileInfo.append(samplerate)
 
+
 tkinterGUIthread = threading.Thread(target=displayGUI)
 tkinterGUIthread.start()
-matplobLib()
 
+matplobLib()
--- audioFilters.py
+++ audioFilters.py
@@ -7,10 +7,10 @@ import scipy.io.wavfile
 # TODO: Kører koden parralelt ift. kodeflow?
 
 mixingParams = np.array([0.3, 0.25, 0.25, 0.2])
-plainReverbDelay = np.array([853, 1613, 1493, 1153])
+plainReverbDelay = np.array([1553, 1613, 1493, 1153])
 allPassReverbDelay = np.array([223, 443])
-allPassParams = np.array([-0.7, 0.7])
-plainReverbTime = 2
+allPassParams = np.array([-0.7, -0.7])
+plainReverbTime = 0.7
 
 
 def gainFromReverb(inputSound, plainReverbDelay1, samplingFrequency):
@@ -31,8 +31,6 @@ def plainReverb(data, delay, plainGainTotal):
         if i < delay:
             outputSound[i] = data[i]
         else:
-            print("Iteration",i)
-            print("Data",data,"plainGainTotal",plainGainTotal)
             outputSound[i] = data[i]+plainGainTotal*outputSound[i-delay]
 
     return outputSound
@@ -48,7 +46,7 @@ def allPassReverb(data, allPassReverbDelayInput, allPassParamsInput):
         else:
             outputSound[i] = allPassParamsInput*data[i]+data[i-allPassReverbDelayInput]-allPassParamsInput*outputSound[i-allPassReverbDelayInput]
 
-    return data
+    return outputSound
 
 
 def schroedersReverb(soundFile, valueParameters, fileFrequency):
@@ -60,11 +58,13 @@ def schroedersReverb(soundFile, valueParameters, fileFrequency):
     for i in range (len(mixingParams)):
         plainGainTotal.append(gainFromReverb(soundFile, int(plainReverbDelay[i]),fileFrequency))
 
-    for i in range(len(plainReverbDelay)):
-        soundFileHolder = soundFileHolder+plainReverb(soundFile, plainReverbDelay[i], plainGainTotal[i])
+    for i in range(np.size(plainReverbDelay)):
+        # print("passPlainGain",plainGainTotal)
+        # print("elementPassPlainGain",plainGainTotal[i])
+        soundFileHolder = soundFile+plainReverb(soundFile, plainReverbDelay[i], plainGainTotal[0][i])
 
     for i in range(len(allPassReverbDelay)):
-        soundFileHolder = allPassReverb(soundFile, allPassReverbDelay[i], allPassParams[i])
+        soundFileHolder = allPassReverb(soundFileHolder, allPassReverbDelay[i], allPassParams[i])
 
     print("SoundfileEnd:",soundFileHolder)
     print("Returning schroeders reverb")
